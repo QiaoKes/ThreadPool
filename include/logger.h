fe02fef738bcd24ce621376926eb18de0496f00e
QiaoKes <q18814783016@gmail.com>
2020-08-06 20:37:53 +0800

    Refactoring using the template pattern and singleton pattern

--- include/logger.h
+++ include/logger.h
@@ -6,21 +6,29 @@
 #include <sstream>
 #include <mutex>
 #include <string>
+#include <map>
+#include <regex>
+#include <ctime>
+#include <chrono>
+#include <iomanip>
+#include <cassert>
+
+#ifdef WIN32
+#define localtime_r(_TIME, _TM) localtime_s(_TM, _TIME)
+#endif
+
 
 namespace Thread {
-
 namespace Logger {
 
 enum class Level { Debug, Info, Warning, Error, Fatal };
-class LoggerStream;
-class LoggerBase;
-class FileLogger;
-class ConsoleLogger;
 
+template<typename T> class BaseLogger;
 
+template<typename T>
 class LoggerStream : public std::ostringstream {
 public:
-    LoggerStream(LoggerBase&, Level);
+    LoggerStream(BaseLogger<T>&, Level);
     LoggerStream() = delete;
     LoggerStream(const LoggerStream&);
     LoggerStream(LoggerStream&&) = default;
@@ -28,50 +36,107 @@ public:
 
     ~LoggerStream();
 private:
-    LoggerBase& _logger;
-    Level       _level;
+    BaseLogger<T>& _logger;
+    Level          _level;
 };
 
-
-class LoggerBase {
-    friend class LoggerStream;
+template<typename T>
+class BaseLogger {
+    friend class LoggerStream<T>;
 public:
-    LoggerBase() = default;
-    virtual ~LoggerBase() = default;
-    virtual LoggerStream operator()(Level level = Level::Debug);
+    virtual LoggerStream<T> operator()(Level level = Level::Debug);
+
+    template<typename... Args>
+    static T& get_instance(Args&&... args);
+
+protected:  
+    BaseLogger() = default;
+    BaseLogger(BaseLogger&) = delete;
+    BaseLogger& operator=(const BaseLogger&) = delete;
+    virtual ~BaseLogger() = default;
 private:
     std::tm get_local_time();
     void end_line(Level, const std::string&);
     virtual void output(const std::tm&, const std::string&, const std::string&) = 0;
 private:
-    std::mutex  _mtx;
-    std::tm     _localtime;
+    std::mutex   _mtx;
+    std::tm      _localtime;
 };
 
 
-class ConsoleLogger : public LoggerBase {
-public:
+class ConsoleLogger : public BaseLogger<ConsoleLogger> {
+    friend class BaseLogger<ConsoleLogger>;
+private:
     ConsoleLogger() = default;
-    ConsoleLogger(const ConsoleLogger&) = delete;
-    ConsoleLogger(ConsoleLogger&&) = delete;
-    ConsoleLogger& operator=(const ConsoleLogger&) = delete;
-    virtual ~ConsoleLogger() = default;
-private:
     void output(const std::tm&, const std::string&, const std::string&) override;
+    ~ConsoleLogger() = default;
 };
 
-class FileLogger : public LoggerBase {
-public:
-    FileLogger(const std::string& fn = "create_at_" __DATE__ "_" __TIME__ ".log");
-    FileLogger(const FileLogger&) = delete;
-    FileLogger(FileLogger&&) = delete;
-    FileLogger& operator=(const FileLogger&) = delete;
-    virtual ~FileLogger();
+class FileLogger : public BaseLogger<FileLogger> {
+    friend class BaseLogger<FileLogger>;
 private:
+    FileLogger(const std::string& fn = "create_at_" __DATE__ "_" __TIME__ ".log");
     void output(const std::tm&, const std::string&, const std::string&) override;
+    ~FileLogger();
 private:
     std::ofstream _file;
 };
+
+
+template<typename T>
+LoggerStream<T>::LoggerStream(BaseLogger<T>& logger, Level level)
+    : _logger(logger), _level(level) { }
+
+template<typename T>
+LoggerStream<T>::LoggerStream(const LoggerStream<T>& ls)
+    : _logger(ls._logger), _level(ls._level) { }
+
+template<typename T>
+LoggerStream<T>::~LoggerStream() {
+    try {
+        _logger.end_line(_level, this->str());
+    }
+    catch (...) {
+        //...
+    }
+}
+
+
+template<typename T>
+template<typename... Args>
+T& BaseLogger<T>::get_instance(Args&&... args) {
+    static T instance(std::forward<Args>(args)...);
+    return instance;
+}
+
+template<typename T>
+std::tm BaseLogger<T>::get_local_time() {
+    auto now = std::chrono::system_clock::now();
+    auto time_t = std::chrono::system_clock::to_time_t(now);
+    localtime_r(&time_t, &_localtime);
+    return _localtime;
+}
+
+template<typename T>
+LoggerStream<T> BaseLogger<T>::operator()(Level level) {
+    return LoggerStream<T>(*this, level); //创建一个默认安全级别为debug的缓冲区
+}
+
+static const std::map<Level, std::string> lmap = {
+        { Level::Debug, "Debug" },
+        { Level::Info, "Info" },
+        { Level::Warning, "Warning" },
+        { Level::Error, "Error" },
+        { Level::Fatal, "Fatal" },
+};
+
+template<typename T>
+void BaseLogger<T>::end_line(Level level, const std::string& info) {
+    std::unique_lock<std::mutex> lock(_mtx);
+    auto local_time = get_local_time();
+    auto level_str = lmap.find(level)->second;
+    output(local_time, level_str, info);
+}
 
 }
 }
--- src/logger.cc
+++ src/logger.cc
@@ -1,58 +1,7 @@
 #include "logger.h"
-#include <regex>
-#include <ctime>
-#include <chrono>
-#include <map>
-#include <iomanip>
-#include <cassert>
-
-#ifdef WIN32
-#define localtime_r(_TIME, _TM) localtime_s(_TM, _TIME)
-#endif
 
 namespace Thread {
 namespace Logger {
-
-LoggerStream::LoggerStream(LoggerBase& logger, Level level)
-    : _logger(logger), _level(level) { }
-
-LoggerStream::LoggerStream(const LoggerStream& ls)
-    : _logger(ls._logger), _level(ls._level) { }
-
-LoggerStream::~LoggerStream() {
-    try {
-        _logger.end_line(_level, this->str());
-    }
-    catch (...) {
-        //...
-    }
-}
-
-std::tm LoggerBase::get_local_time() {
-    auto now = std::chrono::system_clock::now();
-    auto time_t = std::chrono::system_clock::to_time_t(now);
-    localtime_r(&time_t, &_localtime);
-    return _localtime;
-}
-
-LoggerStream LoggerBase::operator()(Level level) {
-    return LoggerStream(*this, level); //创建一个默认安全级别为debug的缓冲区
-}
-
-static const std::map<Level, std::string> lmap = {
-        { Level::Debug, "Debug" },
-        { Level::Info, "Info" },
-        { Level::Warning, "Warning" },
-        { Level::Error, "Error" },
-        { Level::Fatal, "Fatal" },
-};
-
-void LoggerBase::end_line(Level level, const std::string& info) {
-    std::unique_lock<std::mutex> lock(_mtx);
-    auto local_time = get_local_time();
-    auto level_str = lmap.find(level)->second;
-    output(local_time, level_str, info);
-}
 
 std::ostream& operator<<(std::ostream& stream, const std::tm& tm)
 {
@@ -66,7 +15,8 @@ std::ostream& operator<<(std::ostream& stream, const std::tm& tm)
         << setfill('0') << setw(2) << tm.tm_sec;
 }
 
-void ConsoleLogger::output( const std::tm& tm,
+
+void ConsoleLogger::output(const std::tm& tm,
                             const std::string& level,
                             const std::string& info) {
     std::cout << "[" << tm << "]"
@@ -75,8 +25,8 @@ void ConsoleLogger::output( const std::tm& tm,
     std::cout.flush();
 }
 
-FileLogger::FileLogger(const std::string& filename) 
-    : LoggerBase() {
+FileLogger::FileLogger(const std::string& filename)
+    : BaseLogger() {
     std::regex r("/|:| |>|<|\"|\\*|\\?|\\|");
     auto real_name = std::regex_replace(filename, r, "_");
     _file.open(real_name, std::fstream::out | std::fstream::app | std::fstream::ate);
@@ -87,9 +37,9 @@ FileLogger::~FileLogger()
 {
     try {
         _file.flush();
-        _file.close();  
+        _file.close();
     }
-    catch(...) {
+    catch (...) {
         //...
     }
 }
